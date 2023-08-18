740a67b8451f9a2ed9e0487101dbdfaa54aa5b5a
塩島海里 <misato@shiotoukairinoMacBook-Air.local>
2023-08-19 01:36:21 +0900

    first commit

---
+++ page-about.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Template Name: 概要
+*/
+ get_header();
+  ?>
+
+<main>
+  <section id="about" class="wrapper">
+  <h2 class="sec-title">About</h2>
+  <p>Xxxxx Ashley</p>
+  <ul>
+    <li>2th Floor xxxxx Building x-x-x Nishiazabu, Minato-ku, Tokyo 106-0031 Japan</li>
+    <li>tel: 000-0000-0000</li>
+    <li>url: www.xxxxxx.jp</li>
+    <li>mail: xxx@xxxxxx.jp</li>
+  </ul>
+  <p>プロフィールテキストテキストテキストテキストテキストテキストテキストテキストテキストスト テキストテキストテキストテキ ストテキストテキストテキストテキストテキストテキストテキスト テキストテキストテキストテキストテキストテキストテキスト テキストテキストテキストテキスト</p>
+</section>
+
+</main>
+
+<?php get_footer(); ?>
---
+++ page-contact.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Template Name: お問い合わせ
+*/
+get_header();
+?>
+<main>
+  <h3>Contact</h3>
+  <div class="form-container">
+    <?php echo do_shortcode('[contact-form-7 id="1c94c4d" title="Contact"]'); ?>
+  </div>
+</main>
+<?php get_footer(); ?>
